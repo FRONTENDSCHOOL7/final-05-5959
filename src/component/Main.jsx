5b8ff2e5b669d5894d6a9c6fd7351e1668f63ae3
Meow-Chu <chuyeon0314@gmail.com>
2023-10-13 00:46:10 +0900

    splash 구현

---
+++ src/component/Main.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Main() {
+  return (
+    <div>
+      <div>test01</div>
+      <div>test02</div>
+      <div>test03</div>
+      <div>splash01</div>
+    </div>
+  );
+}
