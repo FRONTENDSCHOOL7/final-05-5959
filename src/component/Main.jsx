e81e77f24a104b921986785d74e4cdaf7eab4a16
Meow-Chu <chuyeon0314@gmail.com>
2023-10-12 18:25:28 +0900

    test03

--- src/component/Main.jsx
+++ src/component/Main.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import React from "react";
 
 export default function Main() {
   return (
-  <div>
-    <div>test01</div>
-    <div>test02</div>
-  </div>    
-  )
+    <div>
+      <div>test01</div>
+      <div>test02</div>
+      <div>test03</div>
+    </div>
+  );
 }
